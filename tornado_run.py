6dea18c37cea952c15ed3168007727fc0507107e
Glenn Vorhes <glennvorhes@gmail.com>
2013-12-21 11:02:20 -0600

    Remove cherrypy run in favor of tornado

---
+++ tornado_run.py
@@ -0,0 +1,9 @@
+__author__ = 'glenn'
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from app import app
+
+http_server = HTTPServer(WSGIContainer(app))
+http_server.listen(80)
+IOLoop.instance().start()
